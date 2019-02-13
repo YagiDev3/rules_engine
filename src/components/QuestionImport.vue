4b6b2b183c27d9a4efd00b9d24fbfc4280475b41
cblack <cblack@lgamerica.com>
2019-02-13 11:19:25 -0500

    removed question autogen

--- src/components/QuestionImport.vue
+++ src/components/QuestionImport.vue
@@ -388,9 +388,6 @@ export default {
     this.sequence = 0
     this.questionId = null
     this.questionToggle = false
-    this.id = ([1e7]+-1e3+-4e3+-8e3+-1e11).replace(/[018]/g, c =>
-        (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16)
-      )
   },
   genGUID () {
       this.id = ([1e7]+-1e3+-4e3+-8e3+-1e11).replace(/[018]/g, c =>
